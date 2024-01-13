a78473c9ee588e5c6abb0d25035cc0711d228988
Shoku338 <59188366+Shoku338@users.noreply.github.com>
2024-01-13 19:28:49 +0700

    code stuck at grapple

--- TiemEngine/GameObject.cpp
+++ TiemEngine/GameObject.cpp
@@ -62,48 +62,51 @@ void GameObject::Render(glm::mat4 globalModelTransform)
 }
 
 int GameObject::detectCollisionAABB(float bx, float by, float bh, float bw) {
-	float aHalfWidth = abs(size.x / 2.0f);
-	float aHalfHeight = abs(size.y / 2.0f);
-	float aQuatWidth = abs(size.x / 4.0f);
-	float aQuatHeight = abs(size.y / 4.0f);
+	
+	if(haveCollision){
+		float aHalfWidth = abs(size.x / 2.0f);
+		float aHalfHeight = abs(size.y / 2.0f);
+		float aQuatWidth = abs(size.x / 4.0f);
+		float aQuatHeight = abs(size.y / 4.0f);
 
-	float bTop = abs(by + (bh / 2));
-	float bBot = abs(by - (bh / 2));
-	float bRig = abs(bx + (bw / 2));
-	float bLef = abs(bx - (bw / 2));
+		float bTop = abs(by + (bh / 2));
+		float bBot = abs(by - (bh / 2));
+		float bRig = abs(bx + (bw / 2));
+		float bLef = abs(bx - (bw / 2));
 
-	// Calculate the eight points for each object
-	float aPoints[8][2] = {
-		{pos.x - aQuatWidth, pos.y + aHalfHeight},   // half top left
-		{pos.x + aQuatWidth, pos.y + aHalfHeight},    // half top right
-		{pos.x + aHalfWidth, pos.y + aQuatHeight},   // upper right
-		{pos.x + aHalfWidth, pos.y - aQuatHeight},    // lower right
-		{pos.x - aQuatWidth, pos.y - aHalfHeight},     // half bottom left
-		{pos.x + aQuatWidth, pos.y - aHalfHeight},   // half bottom right
-		{pos.x - aHalfWidth, pos.y + aQuatHeight},   // upper left
-		{pos.x - aHalfWidth, pos.y - aQuatHeight}    // lower left
-	};
+		// Calculate the eight points for each object
+		float aPoints[8][2] = {
+			{this->pos.x - aQuatWidth, this->pos.y + aHalfHeight},   // half top left
+			{this->pos.x + aQuatWidth, this->pos.y + aHalfHeight},    // half top right
+			{this->pos.x + aHalfWidth, this->pos.y + aQuatHeight},   // upper right
+			{this->pos.x + aHalfWidth, this->pos.y - aQuatHeight},    // lower right
+			{this->pos.x - aQuatWidth, this->pos.y - aHalfHeight},     // half bottom left
+			{this->pos.x + aQuatWidth, this->pos.y - aHalfHeight},   // half bottom right
+			{this->pos.x - aHalfWidth, this->pos.y + aQuatHeight},   // upper left
+			{this->pos.x - aHalfWidth, this->pos.y - aQuatHeight}    // lower left
+		};
 
-	//collision logic
-	int collisionResult = 0;
-	if (aPoints[0][1] >= bBot - 5.f && (aPoints[0][0] <= bLef && aPoints[1][0] >= bRig) && aPoints[0][1] < by) {
-		//cout << "TOP" << endl;
-		return 4;
+		//collision logic
+		
+		if (aPoints[0][1] >= bBot && (aPoints[0][0] <= bRig && aPoints[1][0] >= bLef) && aPoints[0][1] < by) {
+			//cout << "TOP" << endl;
+			return 4;
+		}
+		else if (aPoints[4][1] <= bTop  && (aPoints[4][0] <= bRig && aPoints[5][0] >= bLef) && aPoints[4][1] >= by) {
+			//cout << "BOTTOM" << endl;
+			return 3;
+		}
+		else if (aPoints[2][0] >= bLef && (aPoints[2][1] >= bBot && aPoints[3][1] < bTop) && aPoints[2][0] <= bx) {
+			//cout << "RIGHT" << endl;
+			return 2;
+		}
+		else if (aPoints[6][0] <= bRig && (aPoints[6][1] < bTop && aPoints[6][1] > bBot) && aPoints[6][0] > bx) {
+			//cout << "LEFT" << endl;
+			return 1;
+		}
 	}
-	else if (aPoints[4][1] <= bTop && (aPoints[4][0] <= bRig && aPoints[5][0] >= bLef) && aPoints[4][1] >= by) {
-		//cout << "BOTTOM" << endl;
-		return 3;
-	}
-	else if (aPoints[2][0] >= bLef && (aPoints[2][1] >= bBot && aPoints[3][1] < bTop) && aPoints[2][0] <= bx) {
-		//cout << "RIGHT" << endl;
-		return 2;
-	}	
-	else if (aPoints[6][0] <= bRig && (aPoints[6][1] < bTop && aPoints[6][1] > bBot) && aPoints[6][0] > bx) {
-		//cout << "LEFT" << endl;
-		return 1;
-	}
 
-	return collisionResult;
+	return 0;
 }
 
 bool  GameObject::getCollision() {
--- TiemEngine/Level.cpp
+++ TiemEngine/Level.cpp
@@ -98,11 +98,21 @@ void Level::LevelUpdate(float dt)
 					player->setJump(0);
 					player->velocity.y = 0;
 				}
-				if (resultCol == COLLISION_RIGHT)
+				else if (resultCol == COLLISION_RIGHT)
 				{
 					//game logic here
 					player->Translate(glm::vec3(-0.7, 0, 0));
 					player->velocity.x = 0;
+				}
+				else if (resultCol == COLLISION_LEFT) {
+					// Game logic for collision on the left of player
+					player->Translate(glm::vec3(0.5, 0, 0));
+					player->velocity.x = 0;
+				}
+				else if (resultCol == COLLISION_TOP) {
+					// Game logic for collision at the Top of player
+					player->Translate(glm::vec3(0, -0.5, 0));
+					player->velocity.y = 0;
 				}
 				//collision value
 				//cout << player->getPosX() << ' ' << player->getPosY() << ' ' << objectsList.at(i)->getPosX() << ' ' << objectsList.at(i)->getPosY() << ' '<< resultCol << endl;
@@ -120,46 +130,51 @@ void Level::LevelUpdate(float dt)
 				continue;  // Skip the rest of the loop for this iteration
 			}
 		}
-		if (Grapple* grapple = dynamic_cast<Grapple*>(*it)) {
+		else if (Grapple* grapple = dynamic_cast<Grapple*>(*it)) {
 			// Check for collision with other GameObjects
-			for (auto otherIt = objectsList.begin(); otherIt != objectsList.end(); ++otherIt) {
+			for (auto otherIt = objectsList.begin(); otherIt != objectsList.end(); ++otherIt) 
+			{
 				if (GameObject* gameObject = dynamic_cast<GameObject*>(*otherIt)) {
-					if (gameObject->getCollision()) {
-						cout << "PULL" << endl;
-						int resultCol = grapple->detectCollisionAABB(
-							gameObject->getPosX(), gameObject->getPosY(),
-							abs(gameObject->getsizeY()), gameObject->getsizeX());
-						if (resultCol != 0) {
-							// Collision detected, execute pull function
-							//grapple->velocity = glm::vec3(0, 0, 0);
-							//grapple->pull(*player, dt, 70);
-							// Handle other logic if needed
-						}
+					if (grapple->detectCollisionAABB(
+						gameObject->getPosX(), gameObject->getPosY(),
+						abs(gameObject->getsizeY()), gameObject->getsizeX()) != 0) {
+						// Collision detected, execute pull function
+						grapple->velocity = glm::vec3(0, 0, 0);
+						//grapple->pull(*player, dt, 70);
+						// Handle other logic if needed
 					}
 				}
 			}
 		}
-		if (GameObject* gameObject = dynamic_cast<GameObject*>(*it)) {
+		else if (GameObject* gameObject = dynamic_cast<GameObject*>(*it)) {
 			//gameObject->Translate(gameObject->velocity);
-			if (gameObject->getCollision()) {
-				int resultCol = player->detectCollisionAABB(
-					gameObject->getPosX(), gameObject->getPosY(),
-					abs(gameObject->getsizeY()), gameObject->getsizeX());
-
+			int resultCol = player->detectCollisionAABB(
+				gameObject->getPosX(), gameObject->getPosY(),
+				abs(gameObject->getsizeY()), gameObject->getsizeX());
 				if (resultCol == COLLISION_BOTTOM) {
-					// Game logic for collision at the bottom
+					// Game logic for collision at the bottom of player
 					player->setGround(true);
 					player->setJump(0);
 					player->velocity.y = 0;
 				}
 				else if (resultCol == COLLISION_RIGHT) {
-					// Game logic for collision on the right
-					player->Translate(glm::vec3(-0.7, 0, 0));
+					// Game logic for collision on the right of player
+					player->Translate(glm::vec3(-0.5, 0, 0));
 					player->velocity.x = 0;
+				}
+				else if (resultCol == COLLISION_LEFT) {
+					// Game logic for collision on the left of player
+					player->Translate(glm::vec3(0.5, 0, 0));
+					player->velocity.x = 0;
+				}
+				else if (resultCol == COLLISION_TOP) {
+					// Game logic for collision at the Top of player
+					player->Translate(glm::vec3(0, -0.5, 0));
+					player->velocity.y = 0;
 				}
 
 				// Additional collision handling logic can be added here
-			}
+			
 		}
 		++it;  // Increment the iterator for the next iteration
 	}
@@ -273,6 +288,7 @@ void Level::HandleMouse(int type, int x, int y)
 		// Create a new bullet and set its direction
 		Bullet* newBullet = new Bullet(bulletStartPosition, "../Resource/Texture/bullet3.png",150.f);
 		newBullet->SetSize(20.f, 20.f);
+		newBullet->setCollision(false);
 		// Shoot the bullet in the calculated direction
 		newBullet->shootAt(glm::vec2(realX, realY), newBullet->getVelocity().x);
 		objectsList.push_back(newBullet);
@@ -280,6 +296,7 @@ void Level::HandleMouse(int type, int x, int y)
 	else if (type == 1) {
 		Grapple* grapple = new Grapple(bulletStartPosition, "../Resource/Texture/temp-grapple.png",75.f);
 		grapple->SetSize(20.f, 20.f);
+		grapple->setCollision(false);
 		// Shoot the bullet in the calculated direction
 		grapple->shootAt(glm::vec2(realX, realY), grapple->getVelocity().x);
 		objectsList.push_back(grapple);
