e1dff23043771943d6f5d1ba9df662a82ce650f4
NetherCather <mifgwin1234@gmail.com>
2024-02-12 15:18:33 +0700

    fine tune movement

--- TiemEngine/weapon.cpp
+++ TiemEngine/weapon.cpp
@@ -1,4 +1,4 @@
-#include "ProtoPlasma.h"
+#include "weapon.h"
 
 void Weapon::update(glm::vec3 Playerpos) {
     this->pos = Playerpos;
@@ -6,7 +6,7 @@ void Weapon::update(glm::vec3 Playerpos) {
 void Weapon::Fire(glm::vec2 targetPosition, vector<DrawableObject*>& objectsList, SoundManager* soundManager) {
     glm::vec3 bulletStartPosition = this->getPosition() + glm::vec3(10.0f, 20.0f, 0.0f); // Adjust the offset as needed
     Bullet* newBullet = new Bullet(bulletStartPosition, "../Resource/Texture/bullet3.png", 150.f);
-    newBullet->SetSize(20.f, 20.f);
+    newBullet->SetSize(20.f, 20.f); 
     newBullet->shootAt(targetPosition, newBullet->getVelocity().x);
     objectsList.push_back(newBullet);
 
